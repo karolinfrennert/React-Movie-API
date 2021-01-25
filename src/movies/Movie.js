5d87a5e0c6039c18cb25ea962e04a841408fb500
karolinfrennert <karolinfrennert@gmail.com>
2021-01-25 22:36:49 +0100

    Added movie details

--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export function Movie({ movie, config }) {
   return (
     <li>
-      <Link to="/details">
+      <Link to={`/movie/${movie.id}`}>
         {config.images?.base_url && (
           <img
             src={config.images.base_url + "w342" + movie.poster_path}
@@ -20,7 +20,14 @@ export function Movie({ movie, config }) {
 
 Movie.propTypes = {
   movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     poster_path: PropTypes.string.isRequired,
   }).isRequired,
+
+  config: PropTypes.shape({
+    images: PropTypes.shape({
+      base_url: PropTypes.string,
+    }),
+  }),
 };
--- src/movies/MovieDetail.js
+++ src/movies/MovieDetail.js
@@ -1,11 +1,39 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
+const API_KEY = `?api_key=${process.env.REACT_APP_API_KEY}`;
 
-export function MovieDetail(){
-    return(
-        <div>
-            <h1>
-                Movie detail!
-            </h1>
-        </div>
-    )
+const BASE_URL = `https://api.themoviedb.org/3/movie/`;
+
+export function MovieDetail() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+
+  const getMovie = async () => {
+    try {
+      const res = await fetch(BASE_URL + id + API_KEY);
+      const newMovie = await res.json();
+      setMovie(newMovie);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, [id]);
+
+  if (!movie.title) return null;
+
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+      <p>{movie.overview}</p>
+      <ul>
+        {movie.genres.map(({ name, id }) => (
+          <li key={id}> {name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
